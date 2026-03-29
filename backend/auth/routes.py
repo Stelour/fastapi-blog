ccf3ec20b326b6e4be10c89be5270c3bb465dda9
Stelour <stelourgg@gmail.com>
2026-03-30 03:20:21 +0500

    add auth routes

--- backend/auth/routes.py
+++ backend/auth/routes.py
@@ -1,7 +1,144 @@
-from fastapi import APIRouter
+import os
+from dotenv import load_dotenv
+
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pwdlib import PasswordHash
+import jwt
+from jwt.exceptions import InvalidTokenError
+
+from backend.db import get_db
+from backend.models import User, Profile
+from backend.schemas import Token, UserSchema, RegisterRequest, RegisterResponse
+
+from sqlalchemy import select, or_
+from sqlalchemy.ext.asyncio import AsyncSession
+
 
 router = APIRouter(tags=["auth"])
 
-@router.get("/")
-async def root():
-    return {"message": "."}
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("No SECRET_KEY set for FastAPI application")
+
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+password_hash = PasswordHash.recommended()
+
+DUMMY_HASH = password_hash.hash("dummypassword")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+async def get_user_by_username(db: AsyncSession, username: str):
+    result = await db.execute(select(User).where(User.username == username))
+    return result.scalar_one_or_none()
+
+
+async def authenticate_user(db: AsyncSession, login: str, password: str):
+    query = select(User).where(
+        or_(User.username == login, User.email == login)
+    )
+    result = await db.execute(query)
+    user = result.scalars().first()
+
+    if not user:
+        verify_password(password, DUMMY_HASH)
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    db: Annotated[AsyncSession, Depends(get_db)],
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+    user = await get_user_by_username(db, username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@router.post("/login", response_model=Token)
+async def login(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        db: AsyncSession = Depends(get_db)):
+    user = await authenticate_user(db, form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username},
+        expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post("/register", response_model=RegisterResponse, status_code=201)
+async def register(data: RegisterRequest, db: AsyncSession = Depends(get_db)):
+    query = select(User).where(
+        or_(User.username == data.username, User.email == data.email)
+    )
+    result = await db.execute(query)
+    user = result.scalars().first()
+    if user:
+        raise HTTPException(status_code=400, detail="User with this email or username already exists")
+
+    new_user = User(
+        email=data.email,
+        username=data.username,
+        hashed_password=get_password_hash(data.password)
+    )
+
+    new_user.profile = Profile()
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+
+    return {"user_id": new_user.id, "username": new_user.username, "email": new_user.email}
+
+@router.get("/me", response_model=UserSchema)
+async def read_users_me(current_user: Annotated[UserSchema, Depends(get_current_user)]):
+    return current_user
