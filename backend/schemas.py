4bdb22b3eba77858fc55eb9397edf51d855b101a
Stelour <stelourgg@gmail.com>
2026-03-30 03:20:42 +0500

    update auth shemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -5,9 +5,6 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
-class TokenData(BaseModel):
-    username: str | None = None
-
 class UserSchema(BaseModel):
     id: int
     username: str
@@ -20,6 +17,7 @@ class RegisterRequest(BaseModel):
     username: str = Field(min_length=3, max_length=50)
     password: str = Field(min_length=8, max_length=32)
 
-class LoginRequest(BaseModel):
-    login: str = Field(min_length=3, max_length=255)
-    password: str = Field(min_length=8, max_length=64)
+class RegisterResponse(BaseModel):
+    user_id: int
+    username: str
+    email: EmailStr
