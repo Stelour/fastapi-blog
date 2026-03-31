d38978d04e8d73a509c8201b12ccbcd6708d4602
Stelour <stelourgg@gmail.com>
2026-03-31 05:32:33 +0500

    add routes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.base.routes import router as base_router
+from backend.posts.routes import router as posts_router
 from backend.auth.routes import router as auth_router
+from backend.profile.routes import router as profile_router
 
 def create_app():
     blog_app = FastAPI(title="fastapi-blog")
@@ -17,8 +18,9 @@ def create_app():
         allow_headers=["*"],
     )
 
-    blog_app.include_router(base_router)
     blog_app.include_router(auth_router, prefix="/auth")
+    blog_app.include_router(profile_router, prefix="/profiles")
+    blog_app.include_router(posts_router, prefix="/posts")
 
     return blog_app
 
